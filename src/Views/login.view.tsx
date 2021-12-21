a4de47783173ee7f4205ff1dc74282d561b623b7
Stefan <tofanstefan99@gmail.com>
2021-12-21 18:06:39 +0200

    Work in progress

--- src/Views/login.view.tsx
+++ src/Views/login.view.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Wave from "../SVG/wave.svg";
 const Login = () => {
   return (
     <MainContainer>
-      <LoginContainer>I am on the login page </LoginContainer>
+      <LoginContainer>I am on the login page</LoginContainer>
+      <FooterImg src={Wave} alt="wave" />
     </MainContainer>
   );
 };
@@ -13,15 +15,24 @@ const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  background-color: #e5e5e5;
 `;
 
 const LoginContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
   width: 600px;
+  height: 400px;
   margin: auto;
   border: 2px solid red;
   @media (max-width: 768px) {
     width: 95%;
   }
 `;
+
+const FooterImg = styled.img`
+  position: absolute;
+  bottom: 0;
+`;
+export default Login;
