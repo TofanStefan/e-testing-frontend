d8a066f440064f4aea27c1b614e8e336a96242fa
unknown <stefan.tofan@assist.ro>
2021-12-21 16:21:57 +0200

    Started writing styled components

---
+++ src/Views/login.view.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+const Login = () => {
+  return (
+    <MainContainer>
+      <LoginContainer>I am on the login page </LoginContainer>
+    </MainContainer>
+  );
+};
+
+const MainContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 600px;
+  margin: auto;
+  border: 2px solid red;
+  @media (max-width: 768px) {
+    width: 95%;
+  }
+`;
+export default Login;
